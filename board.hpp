ddc0006c05d30b7b30407cbb2384c1cbbab834a2
olly-evans <evansoliver345@gmail.com>
2026-04-10 13:14:17 +0100

    initial commit

--- board.hpp
+++ board.hpp
@@ -8,24 +8,26 @@ class Board {
 
 private:
     // bitboards.
-    
+
 public:
+    // constructor.
+
     void smfl_window_init() {
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME);
-    sf::RectangleShape square(sf::Vector2f(300.f, 300.f)); // width, height
-    square.setPosition(0.f, 0.f);                        // x, y
-    square.setFillColor(sf::Color::Red);
+        sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_NAME);
+        sf::RectangleShape square(sf::Vector2f(300.f, 300.f)); // width, height
+        square.setPosition(0.f, 0.f);                        // x, y
+        square.setFillColor(sf::Color::Red);
 
 
-    sf::Event event;
-    while (window.isOpen()) {
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
+        sf::Event event;
+        while (window.isOpen()) {
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed) window.close();
+            }
+            window.clear();
+            window.draw(square);
+            window.display();
         }
-        window.clear();
-        window.draw(square);
-        window.display();
-    }
-};
+    };
 };
 
