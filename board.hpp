aa06ef66f2c41dfb9d35c0037a944c1f6d0a18db
olly-evans <evansoliver345@gmail.com>
2026-04-10 14:38:50 +0100

    added basic die() function

--- board.hpp
+++ board.hpp
@@ -4,6 +4,7 @@
 
 #define WINDOW_HEIGHT 1280
 #define WINDOW_WIDTH 1280
+
 #define WINDOW_NAME "Chess"
 
 class Board {
@@ -12,11 +13,14 @@ private:
     const unsigned int win_h;
     const unsigned int win_w;
     const std::string win_name;
+
+    const unsigned int GRID_SZ = 8;
+
     unsigned int square_size;
 
     sf::RenderWindow window;
 
-    // bitboards.
+    // Bitboards.
 
 
 
@@ -25,15 +29,25 @@ public:
     Board(const unsigned int ww, const unsigned int wh, const std::string wn) : 
     win_w(ww), win_h(wh), win_name(wn), window(sf::VideoMode(win_w, win_h), win_name){};
 
+    void die(std::string err) {
+        window.close();
+        std::cerr << err << std::endl;
+        exit(1);
+    }
+
     void init() {
         
-        // get_square_size(&square_size);
+        get_square_size(&square_size, win_h, win_w);
+        std::cout << square_size << std::endl;
         // draw_squares();
         // draw_starting_board();
 
     }
 
     const unsigned int *get_square_size(unsigned int *sz, const unsigned win_h, const unsigned win_w) {
+        if (!(win_w == win_h)) die("Window must be square!");
+
+        *sz = win_h/GRID_SZ;
 
         return sz;
     }
