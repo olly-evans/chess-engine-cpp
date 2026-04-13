9becfd11c406415a9febee96af44d2266c22e70d
olly-evans <evansoliver345@gmail.com>
2026-04-13 15:38:23 +0100

    brainfuck piece and drawing with window fuckery

--- pieces.hpp
+++ pieces.hpp
@@ -2,6 +2,32 @@
 
 #include "board.hpp"
 
-class Piece {};
+class Piece{
+protected:
+    int square_index;
+    int x,y;
 
-class Bishop:public Piece {};
+    sf::Texture texture;
+    sf::Sprite sprite;
+
+    sf::RenderWindow& window;
+
+
+
+public:
+    Piece(sf::RenderWindow& w) : window(w) {}
+
+    virtual void draw(sf::RenderWindow window);
+
+};
+
+class Bishop:public Piece {
+
+    void draw(sf::RenderWindow window) {
+        sf::Texture texture;
+        texture.loadFromFile("assets/bK.png");
+        sf::Sprite sprite(texture);
+        sprite.setPosition(sf::Vector2f(2, 2));
+        window.draw(sprite);
+    };
+};
