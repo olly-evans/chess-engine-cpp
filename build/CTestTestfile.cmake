76bb8a6ac343d3b111f331664c9e7d76ae564c34
olly-evans <evansoliver345@gmail.com>
2026-04-17 13:21:12 +0100

    removing build/ from cache

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/olly/Documents/chess
-# Build directory: /home/olly/Documents/chess/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
