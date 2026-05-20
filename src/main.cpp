79f3cf354b8e3763b02d77d024ed6021f1e4cc1e
olly-evans <evansoliver345@gmail.com>
2026-05-20 17:01:43 +0100

    comments in readme.md

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,9 @@
 #include "board.hpp"
 #include "sfml_app.hpp"
 
-const std::string FEN = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
-// const std::string FEN = "8/8/8/4k3/8/4P3/4K3/8 w - - 0 1";
-// const std::string FEN = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
-// const std::string FEN = "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1";
+const std::string FEN = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"; // Complex check check.
+// const std::string FEN = "8/8/8/4k3/8/4P3/4K3/8 w - - 0 1"; // King-pawn endgame.
+// const std::string FEN = "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1"; // Discovered en passant check.
 
 int main() {
 
@@ -15,6 +14,6 @@ int main() {
 
     app.init();
     app.run();
-    
+
     return 0;
 }
